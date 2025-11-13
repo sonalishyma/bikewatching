22b6384e629a3ef640167655cfdf12acb9ebfa13
sonalishyma <sssingh@ucsd.edu>
2025-11-13 14:16:48 -0800

    help

---
+++ map.js
@@ -0,0 +1,6 @@
+console.log("map.js file is running!");
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
