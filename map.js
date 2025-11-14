db014705ea0f5e2ea1c69354cfd52fbb04d2aad9
sonalishyma <sssingh@ucsd.edu>
2025-11-13 16:58:56 -0800

    t

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-console.log("map.js file is running!");
+console.log('map.js file is running!');
 
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
@@ -11,13 +11,13 @@ mapboxgl.accessToken = 'YOUR_TOKEN_HERE';  // keep yours
 
 // 2. Create the map
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',   // REQUIRED
-    center: [-117.1611, 32.7157],  // San Diego or Boston
-    zoom: 12
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12', // REQUIRED
+  center: [-117.1611, 32.7157],                // or Boston, etc.
+  zoom: 12,
 });
 
 // Log when map loads
 map.on('load', () => {
-    console.log('🗺️ Map is fully loaded!');
+  console.log('🗺️ Map is fully loaded!');
 });
